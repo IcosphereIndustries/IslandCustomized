8add613f4a9a3fcf6254c2e0fa9eb850055effcf
Prae <praenyth@gmail.com>
2023-06-12 18:02:11 -0400

    fixed one tiny issue

--- src/main/java/industries/icosphere/islandcustomized/IslandCustomized.java
+++ src/main/java/industries/icosphere/islandcustomized/IslandCustomized.java
@@ -2,10 +2,14 @@ package industries.icosphere.islandcustomized;
 
 import industries.icosphere.islandcustomized.IslandCustomizedConfig;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class IslandCustomized implements ModInitializer {
     public static final IslandCustomizedConfig config = IslandCustomizedConfig.createAndLoad();
     public static final IslandTreasureMap map = new IslandTreasureMap();
+
+    public static Logger logger = LoggerFactory.getLogger("IslandCustomized");
 
     /**
      * Runs the mod initializer.
@@ -13,7 +17,7 @@ public class IslandCustomized implements ModInitializer {
     @Override
     public void onInitialize() {
 
-        // TODO: Delete this next commit
-        System.out.println("Hahahahah i'm using this on Purpose Hahahahahahahahaha");
+        logger.info("mod minecraft :thumebs up:");
+
     }
 }
--- src/main/java/industries/icosphere/islandcustomized/utils/MCCUtils.java
+++ src/main/java/industries/icosphere/islandcustomized/utils/MCCUtils.java
@@ -10,39 +10,52 @@ import java.util.regex.Pattern;
 public class MCCUtils {
 
     public static IslandGameMode getCurrentGame() {
+
         if (MinecraftClient.getInstance().player.getScoreboard() != null) {
+
             // Get first objective from scoreboard, that contains the location data
             Scoreboard scoreboard = MinecraftClient.getInstance().player.getScoreboard();
             String locationData = null;
-            // TODO: Okay this is pretty goofy (there's only a single objective)
+
             for (ScoreboardObjective objective : scoreboard.getObjectives()) {
+
                 locationData = objective.getDisplayName().getString();
+
             }
 
             Pattern pattern = Pattern.compile("MCCI: (.+)", Pattern.DOTALL);
             Matcher matcher = pattern.matcher(locationData);
 
             if (matcher.find()) {
+
                 return switch (matcher.group(1).strip()) {
+
                     case "HOLE IN THE WALL" -> IslandGameMode.HITW;
                     case "BATTLE BOX" -> IslandGameMode.BATTLE_BOX;
                     case "TGTTOS" -> IslandGameMode.TGTTOS;
                     case "SKY BATTLE" -> IslandGameMode.SKY_BATTLE;
                     case "PARKOUR WARRIOR" -> IslandGameMode.PARKOUR_WARRIOR;
                     default -> IslandGameMode.UNKNOWN;
+
                 };
+
             }
+
         }
+
         return IslandGameMode.UNKNOWN;
+
     }
 
     public static boolean isOnMCCi() {
+
         if (MinecraftClient.getInstance().getCurrentServerEntry() == null) {
             // Player is not on a server.
             return false;
         }
 
         return MinecraftClient.getInstance().getCurrentServerEntry().address.contains("mccisland.net");
+
     }
 
 }
